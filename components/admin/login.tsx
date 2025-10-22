086d162e2090fa06536d929c4ded5997227f0a63
ashokverma-an <ashokverma.an.2001@gmail.com>
2025-10-22 08:42:45 +0530

    done by ashok verma

--- components/admin/login.tsx
+++ components/admin/login.tsx
@@ -122,6 +122,7 @@ export function AdminLogin({ onLogin }: LoginProps) {
                 className="w-full bg-gradient-to-r from-blue-600 to-purple-600 hover:from-blue-700 hover:to-purple-700"
                 disabled={isLoading}
               >
+                
                 {isLoading ? 'Authenticating...' : 'Login'}
               </Button>
             </form>
